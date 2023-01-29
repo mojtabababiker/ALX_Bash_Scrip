fe2a1b1e0b02446baad5cd6b2a3f2619a0581d20
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-01-29 14:05:53 -0800

    init the git loclally

---
+++ C_Script/first_C_script.c
@@ -0,0 +1,6 @@
+/*This is the first C lang program ask the user for his/her name and 
+ * compairs it to it's list of user */
+#include <stdio.h>
+
+void compair_fun(char ser_name[] , char password[]){
+	
